c3443ebc306dcf78e4cece5a9464b628f7b9e1d6
Nilay Das <mailnilaydas@gmail.com>
2021-03-01 15:57:51 +0530

    final commit

---
+++ todo-web-app/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TODO (ID,DESC,IS_DONE,TARGET_DATE,USER) values(1,'Learning Spring',false,null,'user1')
+insert into TODO (ID,DESC,IS_DONE,TARGET_DATE,USER) values(2,'Learning java',true,null,'user2')
+insert into TODO (ID,DESC,IS_DONE,TARGET_DATE,USER) values(3,'Learning Node',false,null,'user3')
