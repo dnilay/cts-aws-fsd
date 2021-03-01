20064ba89b5abe8ef30b5e452a9732632e3c1ccf
Nilay Das <mailnilaydas@gmail.com>
2021-03-01 15:36:21 +0530

    added data.sql

--- todo-web-app/src/main/java/com/example/demo/TodoWebAppApplication.java
+++ todo-web-app/src/main/java/com/example/demo/TodoWebAppApplication.java
@@ -10,7 +10,7 @@ import com.example.demo.dao.TodoDao;
 import com.example.demo.model.Todo;
 
 @SpringBootApplication
-public class TodoWebAppApplication implements CommandLineRunner {
+public class TodoWebAppApplication /*implements CommandLineRunner*/ {
 
 	private TodoDao todoDao;
 	
@@ -23,14 +23,15 @@ public class TodoWebAppApplication implements CommandLineRunner {
 		SpringApplication.run(TodoWebAppApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		todoDao.save(new Todo(1, "Demo user1","demo desc-1", new Date(), false));
-		todoDao.save(new Todo(2, "Demo user2","demo desc-2", new Date(), false));
-		todoDao.save(new Todo(3, "Demo user3","demo desc-3", new Date(), false));
-		todoDao.save(new Todo(4, "Demo user4","demo desc-4", new Date(), false));
-		todoDao.save(new Todo(5, "Demo user5","demo desc-5", new Date(), false));
-		
-	}
+	/*
+	 * @Override public void run(String... args) throws Exception { todoDao.save(new
+	 * Todo(1, "Demo user1","demo desc-1", new Date(), false)); todoDao.save(new
+	 * Todo(2, "Demo user2","demo desc-2", new Date(), false)); todoDao.save(new
+	 * Todo(3, "Demo user3","demo desc-3", new Date(), false)); todoDao.save(new
+	 * Todo(4, "Demo user4","demo desc-4", new Date(), false)); todoDao.save(new
+	 * Todo(5, "Demo user5","demo desc-5", new Date(), false));
+	 * 
+	 * }
+	 */
 
 }
