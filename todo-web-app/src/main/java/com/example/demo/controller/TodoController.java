3f957aceb360376391550df378b627a4b1b6c4ce
Nilay Das <mailnilaydas@gmail.com>
2021-03-01 11:10:06 +0530

    initial commit

---
+++ todo-web-app/src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.example.demo.service.TodoService;
+
+@Controller
+@SessionAttributes("name")
+public class TodoController {
+	
+
+	private TodoService service;
+	
+	public TodoController(TodoService service) {
+		super();
+		this.service = service;
+	}
+
+	@RequestMapping(value="/list-todos", method = RequestMethod.GET)
+	public String showTodos(ModelMap model){
+		String name = (String) model.get("name");
+		model.put("todos", service.retrieveTodos(name));
+		return "list-todos";
+	}
+}
