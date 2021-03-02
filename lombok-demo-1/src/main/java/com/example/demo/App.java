0e7bbc9d33a978975d3cd5b5093be6111608e09c
Nilay Das <mailnilaydas@gmail.com>
2021-03-02 14:34:51 +0530

    added logback

--- lombok-demo-1/src/main/java/com/example/demo/App.java
+++ lombok-demo-1/src/main/java/com/example/demo/App.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import java.util.UUID;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App 
 {
     public static void main( String[] args )
@@ -10,6 +12,7 @@ public class App
         Employee employee=new Employee("ABCD","John","Doe","john@email.com");
         Employee employee1=new Employee("ABCD","John","Doe","john@email.com");
         System.out.println(employee.equals(employee1));
+        log.info("success");
        
         
     }
