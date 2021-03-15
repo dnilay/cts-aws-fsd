272b1bc30efd421969c1a5bbb276b7598e247915
Nilay <54245048+dnilay@users.noreply.github.com>
2021-03-15 18:54:33 +0530

    Add files via upload

--- spring-security-demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ spring-security-demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.example.demo.service.HomeResourceService;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	private HomeResourceService homeResourceService;
@@ -22,7 +27,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.userDetailsService(homeResourceService);
+		//auth.userDetailsService(homeResourceService);
+		auth.inMemoryAuthentication()
+		.withUser("john")
+		.password("12345")
+		.roles("USER")
+		.and()
+		.withUser("admin")
+		.password("admin")
+		.roles("ADMIN");
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin")
+		.hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("ADMIN","USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+		
+		
+		
 	}
 
 	
